740714afeef72f9cf2975c386ec49e4345f5b920
Jan R.I. B.F. v.W. v.Z. (faerietree) <radagast@ciry.at>
2016-07-24 21:24:23 +0200

    Support conversion from Mesh to parametric shape formats stp,brp,igs.

--- freecad_convert.py
+++ freecad_convert.py
@@ -47,7 +47,7 @@ FREECAD_LIB_PATH = '/usr/lib/freecad/lib'# path to your FreeCAD.so or FreeCAD.dl
 
 import sys
 sys.path.append(FREECAD_LIB_PATH)#<-- added, otherwise FreeCAD is not found
-
+import os
 import FreeCAD
 import Part
 import Mesh
@@ -58,12 +58,58 @@ import Mesh
 # The original author wrote this script. The syntax was figured out by recording and evaluating some macros in FreeCAD. Thanks open source movement.
 
 
-in_fn, out_fn = sys.argv[1], sys.argv[2]  #<-- repaired, out of bounds
+in_f, out_f = sys.argv[1], sys.argv[2]  #<-- repaired, out of bounds
+in_fn,in_ext = os.path.splitext(in_f)
+out_fn,out_ext = os.path.splitext(out_f)
+print(in_ext, " -> ", out_ext)
 
-Part.open(in_fn)
-o = [ FreeCAD.getDocument("Unnamed").findObjects()[0] ]
-Mesh.export(o, out_fn)
+mesh_formats = ['.dae', '.stl', '.obj']
 
+def main():
+    shape = Part.Shape()
+    #shape_formats = ['.brp', '.igs', '.stp']
+    if in_ext in mesh_formats:
+        print("Opening mesh file: ", in_f)
+        Mesh.open(in_f)
+        o = FreeCAD.getDocument("Unnamed").findObjects()[0]
+        #print("dir: ", dir(o))
+        if out_ext in mesh_formats:
+            print("Exporting to mesh file: ", out_f)
+            Mesh.export([o], out_f)
+        else:
+            # TODO This is not optimizing the resulting amount of faces!
+            # see http://www.freecadweb.org/wiki/index.php?title=Mesh_to_Part
+            shape.makeShapeFromMesh(o.Mesh.Topology, 0.05)  # tolerance for sewing
+            exportParametric(shape, out_f, out_ext)
+    elif out_ext in mesh_formats:
+        print("Opening parametric file: ", in_f)
+        Part.open(in_f)
+        o = FreeCAD.getDocument("Unnamed").findObjects()[0]
+        print("Exporting to mesh file: ", out_f)
+        Mesh.export([o], out_f)
+    else:
+        # Parametric -> Parametric
+        print("Opening parametric file: ", in_f)
+        shape.read(in_f)
+        exportParametric(shape, out_f, out_ext)
+
+
+
+def exportParametric(shape, out_f, out_ext):
+    print("Exporting to parametric file: ", out_f)
+    if out_ext == '.brp':
+        shape.exportBrep(out_f)
+    elif out_ext == '.igs':
+        shape.exportIges(out_f)
+    elif out_ext == '.stl':
+        shape.exportStl(out_f)
+    elif out_ext == '.stp':
+        shape.exportStep(out_f)
+    else:
+        print("Export to '%s' not supported." % (out_ext))
+
+
+main()
 
 '''
 def import_fcstd(filename):
@@ -92,8 +138,8 @@ def import_fcstd(filename):
 
 def main():
    pass
-#   Blender.Window.FileSelector(import_fcstd, 'IMPORT FCSTD', 
-#                       Blender.sys.makename(ext='.fcstd'))    
+#   Blender.Window.FileSelector(import_fcstd, 'IMPORT FCSTD',
+#                       Blender.sys.makename(ext='.fcstd'))
 
 # This lets you import the script without running it
 if __name__=='__main__':
